3530e37cbb953c8265af4a3eed74c4eaebc5768d
Jan Vansteenkiste <jan@vstone.eu>
2022-02-04 16:07:07 +0100

    Add some info to the type Php_settings

--- types/php_settings.pp
+++ types/php_settings.pp
@@ -1,3 +1,14 @@
+# Data type to represent php settings:
+#   [ { 'name'  => 'name1',
+#       'value' => 'value1',
+#     },
+#     {
+#       'name'  => ['name2', 'sub'],
+#       'value' => 2',
+#     },
+#     ...
+#   ]
+#
 type Hash2stuff::Php_settings = Array[Struct[{
   name  => Variant[String[1], Array[String[1], 1]],
   value => Any,
