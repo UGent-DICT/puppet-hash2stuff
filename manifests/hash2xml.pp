8b95292df6d041d34eea8496592d1f5299eab41d
Jan Vansteenkiste <jan@vstone.eu>
2022-02-10 13:54:33 +0100

    Add file wrapper for hash2xml

---
+++ manifests/hash2xml.pp
@@ -0,0 +1,38 @@
+# @summary
+#   Defined type provides an implementation of the hash2xml function, creating a XML File.
+#
+# @param file_props
+#   Properties of the target file resource.  Accepts and requires the same parameters of a puppet "file"
+#
+# @param data
+#   A hash to format using xml.
+#
+# @param options
+#   Hash of optional values to pass to the "hash2php" function.  See function for details.
+#
+# @example
+#   hash2stuff::hash2xml { '/path/to/settings.xml':
+#     file_props => {
+#       ensure => file,
+#       owner  => 'root',
+#       group  => 'root',
+#       mode   => '0644',
+#     },
+#     data       => {
+#       'properties' => {
+#         'foo' => 'bar',
+#         'oof' => 'rab',
+#       },
+#     }
+#   }
+#
+define hash2stuff::hash2xml (
+  Hash $file_props,
+  Hash[String, Any] $data = {},
+  Hash $options = {},
+) {
+
+  File {$name:
+    * => merge($file_props, {'content' => hash2xml($data, $options)}),
+  }
+}
